69901709471ff7e5d46b0bcb6c901052a7b98fda
2412pooja1 <pdalvi0203@gmail.com>
2024-11-18 11:31:59 +0530

    changes

--- validations/bookingFormValidation.js
+++ validations/bookingFormValidation.js
@@ -7,12 +7,7 @@ exports.validateBookingForm = [
   body('lname').isString().withMessage('Last name is required and must be a string'),
   body('email').isEmail().withMessage('Invalid email address'),
   body('phone').isString().withMessage('Phone number is required and must be a string'),
-  body('vehicletype').custom(value => {
-    if (!Array.isArray(value)) {
-      throw new Error('Vehicle type should be an array');
-    }
-    return true;
-  }),
+
   body('slotdate').isString().withMessage('Slot date is required'),
   body('slotsession').isString().withMessage('Slot session is required'),
 ];
